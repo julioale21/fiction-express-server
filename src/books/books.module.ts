dfc086ec70f655796c4920e047a97050dd0b22e1
julioale21 <30907777+julioale21@users.noreply.github.com>
2024-10-18 18:03:58 -0300

    exported module

--- src/books/books.module.ts
+++ src/books/books.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { BooksService } from './books.service';
 import { BooksController } from './books.controller';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   controllers: [BooksController],
   providers: [BooksService],
+  imports: [AuthModule],
 })
 export class BooksModule {}
