9ead30a83fded2ba13d246c162b689dce5465d31
julioale21 <30907777+julioale21@users.noreply.github.com>
2024-10-18 17:59:01 -0300

    added route protected with custom decorators

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,16 +1,19 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { BooksService } from './books.service';
+import { Auth } from 'src/auth/decorators';
 
 @Controller('books')
 export class BooksController {
   constructor(private readonly booksService: BooksService) {}
 
   @Get()
+  @Auth()
   findAll() {
     return this.booksService.findAll();
   }
 
   @Get(':id')
+  @Auth()
   findOne(@Param('id') id: string) {
     return this.booksService.findOne(+id);
   }
