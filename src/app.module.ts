460f92e3a82a1b70cded1b28f903437e0714c59b
julioale21 <30907777+julioale21@users.noreply.github.com>
2024-10-20 10:49:07 -0300

    added health check

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { BooksModule } from './books/books.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { HealthController } from './health/health.controller';
 
 @Module({
   imports: [ConfigModule.forRoot(), BooksModule, AuthModule],
-  controllers: [],
+  controllers: [HealthController],
   providers: [],
 })
 export class AppModule {}
